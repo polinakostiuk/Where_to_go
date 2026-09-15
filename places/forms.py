8c24b491e951b7df7816f8841bf4c219b790ff78
polinakostiuk <p.kostiuk@ukma.edu.ua>
2026-09-15 18:35:54 +0300

    add emoji

--- places/forms.py
+++ places/forms.py
@@ -17,23 +17,23 @@ class PlaceForm(forms.Form):
         widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Наприклад: Кав’ярня біля дому'}) 
     )
     description = forms.CharField(
-        label="Опис місця", 
+        label="Опис місця✍🏻", 
         required=True,
         widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Наприклад: Дуже затишна атмосфера', 'rows': 4}) 
     )
     place_type = forms.ChoiceField(
-        label="Тип місця", 
+        label="Тип місця🌄", 
         choices=TYPE_CHOICES,
         widget=forms.Select(attrs={'class': 'form-select'})
     )
     location = forms.CharField(
-        label="Локація / Адреса", 
+        label="Локація / Адреса📍", 
         max_length=150, 
         required=False,
         widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Залиште порожнім для Secret place👀'})
     )
     rating = forms.IntegerField(
-        label="Особистий рейтинг (1-5)", 
+        label="Особистий рейтинг (1-5)🤩", 
         min_value=1, 
         max_value=5,
         widget=forms.NumberInput(attrs={'class': 'form-control', 'min': 1, 'max': 5})
